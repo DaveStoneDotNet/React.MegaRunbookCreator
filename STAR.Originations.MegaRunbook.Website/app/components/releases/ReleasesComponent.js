2605c84da3e38bd8f4df607546888406ccded785
Dave Stone <dave.stone@NationstarMail.com>
2017-02-17 11:26:55 -0600

    Added ignored ReleasesComponent

---
+++ STAR.Originations.MegaRunbook.Website/app/components/releases/ReleasesComponent.js
@@ -0,0 +1,12 @@
+﻿import React    from 'react';
+import { Link } from 'react-router';
+
+const ReleasesComponent = () => {
+    return (
+      <div>
+        RELEASES
+      </div>
+  );
+};
+
+export default ReleasesComponent;
