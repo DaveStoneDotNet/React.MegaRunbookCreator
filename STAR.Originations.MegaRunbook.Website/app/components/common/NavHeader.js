df524ab7b70854829a4077bd74c9f15303ec8b8a
Dave Stone <dave.stone@NationstarMail.com>
2017-06-21 15:45:40 -0500

    Minor Navar Change

--- STAR.Originations.MegaRunbook.Website/app/components/common/NavHeader.js
+++ STAR.Originations.MegaRunbook.Website/app/components/common/NavHeader.js
@@ -19,13 +19,19 @@ class NavHeader extends React.Component {
         return (
                <div className="navbar-root">
                    <div id='navbar' className='navbar navbar-inverse navbar-fixed-top'>
-                     <div className='container'>
+                     <div className='container-fluid'>
                        <div className='navbar-header'>
+                         <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#mainNavbar">
+                           <span className="sr-only">Toggle navigation</span>
+                           <span className="icon-bar"></span>
+                           <span className="icon-bar"></span>
+                           <span className="icon-bar"></span>
+                         </button>
                          <IndexLink className='navbar-brand' to='/'>MRC</IndexLink>
                        </div>
-                       <div className='navbar-collapse collapse'>
+                       <div id="mainNavbar" className='navbar-collapse collapse'>
                           <div className="head-runner">
-                             <img src="../../app//images/running-01.gif" className={app.ajaxCount > 0 ? "show-working-head-runner" : "hide-working-head-runner"}/>
+                             <img src="../../app//images/running-01.gif" className={app.ajaxCount > 0 ? "show-working-head-runner" : "hide-working-head-runner"} alt="working"/>
                           </div>
                          <div className='float-right pad-top-15'>
                            <span className=""><i className="fa fa-user-circle gray-5 font-1-10" /> <span style={{marginTop: "-1px"}} className="Lato font-1-00 opacity-60 pad-left-5 lowercase">{ app.user.UserDisplayName }</span></span>
