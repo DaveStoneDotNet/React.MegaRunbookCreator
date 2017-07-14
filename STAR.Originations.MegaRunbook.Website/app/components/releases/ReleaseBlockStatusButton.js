7b8a63c146c000edb135226c582a5aa90f5b7257
Dave Stone <dave.stone@NationstarMail.com>
2017-07-14 10:37:15 -0500

    Conditional Status Button

---
+++ STAR.Originations.MegaRunbook.Website/app/components/releases/ReleaseBlockStatusButton.js
@@ -0,0 +1,22 @@
+﻿import React  from 'react';
+
+class ReleaseBlockStatusButton extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    componentDidMount () {
+    }
+
+    componentWillUnmount() {
+    }
+
+    render() {
+        return (
+                    <div>
+                    </div>
+               );
+    }
+}
+
+export default ReleaseBlockStatusButton;
