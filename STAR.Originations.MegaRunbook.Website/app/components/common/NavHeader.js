cbec0516e71966d4d919c128be343814b2283000
Dave Stone <daveandkim22@hotmail.com>
2017-07-22 21:46:09 -0500

    More Navbar adjustments. Considering ditching the Bootstrap navbar and creating a custom navbar instead.

--- STAR.Originations.MegaRunbook.Website/app/components/common/NavHeader.js
+++ STAR.Originations.MegaRunbook.Website/app/components/common/NavHeader.js
@@ -25,35 +25,37 @@ class NavHeader extends React.Component {
                 
                 <Navbar inverse fixedTop collapseOnSelect>
                     <div className="container-fluid">
-                         <div className="head-runner">
-                            <img src="../../app//images/running-01.gif" className={app.ajaxCount > 0 ? "show-working-head-runner" : "hide-working-head-runner"} alt="working" />
-                        </div>
-                        <div className="float-right pad-top-15">
-                            <span className=""><i className="fa fa-user-circle gray-5 font-1-10" /> <span style={{ marginTop: "-1px" }} className="Lato font-1-00 opacity-60 pad-left-5 lowercase">{app.user.UserDisplayName}</span></span>
+                        <div className="float-right hidden-sm hidden-xs hidden-md-block visible-lg-block">
+                            <div className="head-runner">
+                                <img src="../../app//images/running-01.gif" className={app.ajaxCount > 0 ? "show-working-head-runner" : "hide-working-head-runner"} alt="working" />
+                            </div>
+                            <div className="float-right pad-top-15">
+                                <span className=""><i className="fa fa-user-circle gray-5 font-1-10" /> <span style={{ marginTop: "-1px" }} className="Lato font-1-00 opacity-60 pad-left-5 lowercase">{app.user.UserDisplayName}</span></span>
+                            </div>
                         </div>
                         <Navbar.Header>
                             <Navbar.Toggle className="navbar-toggle collapsed" data-toggle="collapse" data-target="#mainNavbar" />
-                        <Navbar.Brand>
-                            <Link to='/'>MRC</Link>
-                        </Navbar.Brand>
-                        <Nav id="mainNavbar" className="navbar-collapse collapse">
-                            <LinkContainer to="/" onlyActiveOnIndex><NavItem>Home</NavItem></LinkContainer>
-                            <LinkContainer to="/releases"><NavItem>Release</NavItem></LinkContainer>
-                            <LinkContainer to="/rfcs"><NavItem>RFCs</NavItem></LinkContainer>
-                            <LinkContainer to="/runbooks"><NavItem>Runbooks</NavItem></LinkContainer>
-                            <LinkContainer to="/templates"><NavItem>Templates</NavItem></LinkContainer>
-                            <LinkContainer to="/builds"><NavItem>Builds</NavItem></LinkContainer>
-                            <LinkContainer to="/applications"><NavItem eventKey={7}>Applications</NavItem></LinkContainer>
-                            <LinkContainer to="/admin"><NavItem>Admin</NavItem></LinkContainer>
-                            <LinkContainer to="/about"><NavItem>About</NavItem></LinkContainer>
-                            <NavDropdown title="Demo" id="basic-nav-dropdown">
-                                <LinkContainer to="/demo"><MenuItem>Demo</MenuItem></LinkContainer>
-                                <LinkContainer to="/tba_01"><MenuItem>Design</MenuItem></LinkContainer>
-                                <LinkContainer to=""><MenuItem divider /></LinkContainer>
-                                <LinkContainer to="/tba_02"><MenuItem>React</MenuItem></LinkContainer>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Header>
+                            <Navbar.Brand>
+                                <Link to='/'>MRC</Link>
+                            </Navbar.Brand>
+                            <Nav id="mainNavbar" className="navbar-collapse collapse">
+                                <LinkContainer to="/" onlyActiveOnIndex><NavItem>Home</NavItem></LinkContainer>
+                                <LinkContainer to="/releases"><NavItem>Release</NavItem></LinkContainer>
+                                <LinkContainer to="/rfcs"><NavItem>RFCs</NavItem></LinkContainer>
+                                <LinkContainer to="/runbooks"><NavItem>Runbooks</NavItem></LinkContainer>
+                                <LinkContainer to="/templates"><NavItem>Templates</NavItem></LinkContainer>
+                                <LinkContainer to="/builds"><NavItem>Builds</NavItem></LinkContainer>
+                                <LinkContainer to="/applications"><NavItem eventKey={7}>Applications</NavItem></LinkContainer>
+                                <LinkContainer to="/admin"><NavItem>Admin</NavItem></LinkContainer>
+                                <LinkContainer to="/about" className="hidden-sm hidden-xs visible-md-block visible-lg-block"><NavItem>About</NavItem></LinkContainer>
+                                <NavDropdown title="Demo" id="basic-nav-dropdown">
+                                    <LinkContainer to="/demo"><MenuItem>Demo</MenuItem></LinkContainer>
+                                    <LinkContainer to="/tba_01"><MenuItem>Design</MenuItem></LinkContainer>
+                                    <LinkContainer to=""><MenuItem divider /></LinkContainer>
+                                    <LinkContainer to="/tba_02"><MenuItem>React</MenuItem></LinkContainer>
+                                </NavDropdown>
+                            </Nav>
+                        </Navbar.Header>
                     </div>
                 </Navbar>
                </div>
