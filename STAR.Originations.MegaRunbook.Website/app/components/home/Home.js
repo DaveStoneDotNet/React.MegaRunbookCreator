1456529d74472dbbe37512784d28761cf7c54914
Dave Stone <daveandkim22@hotmail.com>
2017-07-24 15:31:18 -0500

    Created temporary links to layouts.

--- STAR.Originations.MegaRunbook.Website/app/components/home/Home.js
+++ STAR.Originations.MegaRunbook.Website/app/components/home/Home.js
@@ -17,6 +17,14 @@ class Home extends React.Component {
         return (
                     <div className="align-center">
                         <HomeButton appDimensions={appDimensions} />
+                        <div className="pad-40 white-a-1 font-1-50">
+                            <div>
+                                <a href="/design/release-status.html" target="_blank" title="status"><i className="fa fa-circle-thin pad-right-10" /></a> 
+                                <a href="/design/rfcs.html" target="_blank" title="rfcs"><i className="fa fa-circle-thin pad-right-10" /></a>
+                                <a href="/design/runbooks.html" target="_blank" title="runbooks"><i className="fa fa-circle-thin pad-right-5 pad-right-10" /></a>
+                                <a href="/design/app-services.html" target="_blank" title="apps"><i className="fa fa-circle-thin pad-right-5 pad-right-10" /></a>
+                            </div>
+                        </div>
                     </div>
                );
     }
